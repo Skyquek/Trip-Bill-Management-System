61a6293e041d06b9ba36db896c475ac661d6c657
Quek Yao Jing <yaojingquek@gmail.com>
2023-01-02 07:43:54 +0000

    Add tutorial 6 and 7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+# Step 1: Create a model admin class
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+# Step 2: Pass to the second argument of site register
+admin.site.register(Question, QuestionAdmin)
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -11,11 +12,19 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any question that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lt = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
