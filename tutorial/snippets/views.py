049f9338509905f363521d3e5d460e06848a2700
Quek Yao Jing <yaojingquek@gmail.com>
2023-01-08 09:11:33 +0000

    tutorial 3 - Using generics

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,30 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-
-from django.http import Http404
-
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     query_set = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     query_set = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    
