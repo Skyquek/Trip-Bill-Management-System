44ece031414757abf45fe11a37b11be29f41230e
Quek Yao Jing <yaojingquek@gmail.com>
2023-01-29 14:28:10 +0800

    Added the cookbook graphene-graphQL

--- cookbook/ingredients/admin.py
+++ cookbook/ingredients/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ingredients.models import Category, Ingredient
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Ingredient)
--- cookbook/ingredients/models.py
+++ cookbook/ingredients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):    
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return str(self.name)
+
+class Ingredient(models.Model):
+    """
+    """
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="ingredients", on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return str(self.name)
