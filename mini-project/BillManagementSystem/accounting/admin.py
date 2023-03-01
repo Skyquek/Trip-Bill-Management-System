45562e46abe9d4821eabf8ea90d3352342d167ec
Quek Yao Jing <yaojingquek@gmail.com>
2023-03-01 10:45:25 +0000

    Register user to admin for adding data

--- mini-project/BillManagementSystem/accounting/admin.py
+++ mini-project/BillManagementSystem/accounting/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bill, User, Category
 
 # Register your models here.
+admin.site.register(Bill)
+admin.site.register(User)
+admin.site.register(Category)
