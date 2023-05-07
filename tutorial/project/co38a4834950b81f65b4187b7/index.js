1d5b40541b2d4e215633096eb2cbdc16b4158160
Quek Yao Jing <yaojingquek@gmail.com>
2023-05-07 13:52:34 +0800

    Added tutorials for react

---
+++ tutorial/project/co38a4834950b81f65b4187b7/index.js
@@ -0,0 +1,9 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
