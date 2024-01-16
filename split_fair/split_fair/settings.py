cc5969f756b0f8ea5803568261a0dbcbe91b8b6d
Quek Yao Jing <yaojingquek@gmail.com>
2024-01-16 22:29:54 +0800

    register the created app

--- split_fair/split_fair/settings.py
+++ split_fair/split_fair/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'comment',
+    'debt',
+    'expense',
+    'group',
     'user',
 ]
 
